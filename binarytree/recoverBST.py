b588c7ffa771f06a45fc062fe168a7a7b5bd8be7
Nivi-1412 <jyothik.be18@uceou.edu>
2025-06-04 23:49:47 +0530

    feat:BT

--- binarytree/recoverBST.py
+++ binarytree/recoverBST.py
@@ -14,40 +14,49 @@ from binarytree_practice1 import build_binarytree,print_binarytree
 
 def recoverBST(my_list,isadjacent):
 	if isadjacent:
-		prev=my_list[0]
-		for i in range(len(my_list)):
-			if prev>my_list[i]:
-				local=prev
-				prev=my_list[i]
-				my_list[i]=local
+		prev=0
+		for i in range(1,len(my_list),+1):
+			if my_list[prev]>my_list[i]:
+				my_list[prev],my_list[i]=my_list[i],my_list[prev]
+				return
+			prev=i
 	else:
-		
+		prev=my_list[0]
+		count=0
+		for i in range(1,len(my_list),+1):
+			curr=my_list[i]
+			if prev >curr and count==0:
+				swap1=i-1
+				count=1
+			elif prev > curr and count==1:
+				swap2=i
+				my_list[swap1],my_list[swap2]=my_list[swap2],my_list[swap1]
+				return
+			prev=my_list[i]
 
 
-
-
-def isadjacent(my_list):
+def isadjacentfun(my_list):
 	prev=my_list[0]
 	count=0
 	for i in my_list:
 		if prev > i:
 			count+=1
+
 		prev=i
 	if count==1:
 		isadjacent=True
 	else: isadjacent=False
-	recoverBST(my_list,isadjacent)
+	return isadjacent
 
 
-def inordertraversal(root):
+
+def inordertraversal(root,my_list):
 	if root.left!=None:
 		inordertraversal(root.left,my_list)
 	if root!=None:
-		my_list.append(root.data)
+		my_list.append(int(root.data))
 	if root.right!=None:
 		inordertraversal(root.right,my_list)
-	isadjacent(my_list)
-
 
 
 inp=input("enter the nodes of tree seperated by spaces")
@@ -55,3 +64,18 @@ inp=inp.split()
 root=build_binarytree(inp)
 my_list=[]
 inordertraversal(root,my_list)
+print(my_list)
+isadjacent=isadjacentfun(my_list)
+print(isadjacent)
+recoverBST(my_list,isadjacent)
+print(my_list)
+result=[]
+j=0
+for i in range(len(inp)):
+	if inp[i] == 'null':
+		result.append('null')
+	else:
+		result.append(my_list[j])
+		j+=1
+root=build_binarytree(result)
+print_binarytree(root)

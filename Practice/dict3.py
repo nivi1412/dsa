e5f69f8f8d28549c11ca4f4fe9e073caa16d32ff
Nivi-1412 <jyothik.be18@uceou.edu>
2025-06-22 21:21:27 +0530

    feat:practice

--- Practice/dict3.py
+++ Practice/dict3.py
@@ -7,3 +7,50 @@
 
 # Input: root = [1,null,2]
 # Output: 2
+
+class BinaryTree():
+	def __init__(self,data):
+		self.data=data
+		self.left=None
+		self.right=None
+
+def Build_BinaryTree(inp):
+	Arr=[]
+	for i in inp:
+		if i =='null':
+			Arr.append(None)
+		else:
+			Arr.append(BinaryTree(i))
+	for i in range(len(Arr)):
+		if Arr[i]==None:
+			continue
+		if 2*i+1 < len(Arr) and Arr[2*i+1]!=None:
+			Arr[i].left=Arr[2*i+1]
+		if 2*i+2 < len(Arr) and Arr[2*i+2]!=None:
+			Arr[i].right=Arr[2*i+2]
+	return Arr[0]
+
+def Print_BinaryTree(root):
+	if root==None:
+		return
+	print("Node:",root.data)
+	if root.left!=None:
+		print("left:",root.left.data)
+	if root.right!=None:
+		print("right:",root.right.data)
+	Print_BinaryTree(root.left)
+	Print_BinaryTree(root.right)
+
+def Depth_BinaryTree(root):
+	if root==None:
+		return 0
+	return 1+max(Depth_BinaryTree(root.left),Depth_BinaryTree(root.right))
+
+
+inp=input("enter nodes of tree sep by spaces")
+inp=inp.split()
+root=Build_BinaryTree(inp)
+Print_BinaryTree(root)
+print(Depth_BinaryTree(root))
+
+

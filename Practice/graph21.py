1c90e312634ff1c257126c0735a3d446554e1f13
Nivi-1412 <jyothik.be18@uceou.edu>
2025-08-10 12:21:06 +0530

    feat:graph

--- Practice/graph21.py
+++ Practice/graph21.py
@@ -1,7 +1,7 @@
 #graph21.py
 # Cheapest Flights Within K Stops
 import ast
-from collections import deque
+import heapq
 
 n=int(input("no of cities: "))
 flights=input("enter the flights array: ")
@@ -11,31 +11,40 @@ dst=int(input("enter the destination node: "))
 k=int(input("enter no of atmost stops from source to destination: "))
 
 route_map={}
-visited={}
 price=0
+found=False
 
 for flight in flights:
 	if flight[0] not in route_map:
 		route_map[flight[0]]={}
-		visited[flight[0]]=False
-	visited[flight[1]]=False
+		# visited[flight[0]]=False
 	route_map[flight[0]][flight[1]]=flight[2]
-print(route_map)
 
-q=deque()
-q.append([src,0,0])
-visited[src]=True
-i=0
-while q:
-	print(q)
-	src_node,stops_count,prev_price=q.popleft()
-	for next_node,price in route_map.get(src_node,{}).items():
-		if visited[next_node]==False:
-			visited[next_node]=True
-			print(next_node,price)
-			q.append([next_node,stops_count+1,prev_price+price])
+heap=[]
+heapq.heappush(heap,([0,src,0]))
+cheapest_flight={}
+while heap:
+	price,src_node,stops_count=heapq.heappop(heap)
 
+	if src_node not in cheapest_flight:
+		cheapest_flight[src_node]={}
+	if stops_count in cheapest_flight[src_node]:
+		continue
+	cheapest_flight[src_node][stops_count]=price
 
+	for next_node,next_price in route_map.get(src_node,{}).items():
+		if stops_count <= k+1:
+			heapq.heappush(heap,[price+next_price,next_node,stops_count+1])
 
+min_price=10**5
+for node_count,price in cheapest_flight[dst].items():
+	if node_count-1 <= k:
+		if price<min_price:
+			min_price=price
+
+if min_price!=10**5:
+	print(min_price)
+else:
+	print(-1)
 
 
--- Practice/graph22.py
+++ Practice/graph22.py
@@ -6,33 +6,22 @@
 import ast
 import heapq
 
-def detect_cycle(graph,node,visited,finished):
+def detect_cycle(graph,node,visited,finished,memo):
 	cycle=False
 	visited[node]=True
+	if node in memo and memo[node]==True:
+		return True
 	for next_node in graph[node]:
+		print(next_node)
 		if finished[next_node]==False and visited[next_node]==True:
 			return True
 		if visited[next_node]==False:
 			visited[next_node]=True
-			cycle=cycle or detect_cycle(graph,next_node,visited,finished)
-			return cycle
-	return cycle
+			cycle=cycle or detect_cycle(graph,next_node,visited,finished,memo)
+			# return cycle
+	memo[node]=cycle
 	finished[node]=True
-
-
-# def detect_safenodes(graph,node,visited,final_list,my_list):
-# 	def DFS(graph,node,visited,my_list):
-# 		for next_node in graph[node]:
-# 			if visited[next_node]==False:
-# 				visited[next_node]=True
-# 				print("next_node,curr node:",node,next_node)
-# 				if ((next_node in my_list) and (node not in final_list)):
-# 					print("my_list",my_list,next_node)
-# 					heapq.heappush(final_list,node)
-# 				DFS(graph,next_node,visited,my_list)
-# 		return final_list
-# 	return DFS(graph,node,visited,my_list)
-
+	return cycle
 
 inp=input("enter the graph: ")
 inp=ast.literal_eval(inp)
@@ -40,6 +29,7 @@ inp=ast.literal_eval(inp)
 graph={}
 visited={}
 finished={}
+memo={}
 final_list=[]
 safe=False
 
@@ -47,23 +37,17 @@ for i in range(len(inp)):
 	if inp[i] == []:
 		visited[i]=False
 		finished[i]=False
-		heapq.heappush(final_list,i)
 	graph[i]=inp[i]
 	visited[i]=False
 	finished[i]=False
-print(graph)
-print(visited)
 visited_copy=visited.copy()
-my_list=final_list.copy()
+finished_copy=finished.copy()
 
 #detect cycle from each node return node is safe or not
 for node in graph:
-	safe=detect_cycle(graph,node,visited,finished)
-	print(node,safe)
-
-# for node in graph:
-# 	visited=visited_copy.copy()
-# 	visited[node]=True
-# 	final_list=detect_safenodes(graph,node,visited,final_list,my_list)
-
+	visited=visited_copy.copy()
+	finished=finished_copy.copy()
+	cycle=detect_cycle(graph,node,visited,finished,memo)
+	if not cycle:
+		heapq.heappush(final_list,node)
 print(final_list)

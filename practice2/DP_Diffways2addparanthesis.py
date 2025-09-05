2e5faed3f390419afbbff744e57de722b9715749
Nivi-1412 <jyothik.be18@uceou.edu>
2025-09-05 14:16:28 +0530

    feat: modified DP_Diffways2addparanthesis

--- practice2/DP_Diffways2addparanthesis.py
+++ practice2/DP_Diffways2addparanthesis.py
@@ -3,26 +3,26 @@ import ast
 
 def addparentheses(expression,memo)->list:
 	if expression in memo:
-		print(expression)
 		return memo[expression]
 
 	if expression.isdigit():
 		return [int(expression)]
 	else:
 		finallist=[]
-		for i in range(1,len(expression),+2):
-			s1=expression[0:i]
-			s2=expression[i+1:]
-			left=addparentheses(s1,memo)
-			right=addparentheses(s2,memo)
-			for l in left:
-				for r in right:
-					if expression[i]=='+':
-						finallist.append(l+r)
-					if expression[i]=='-':
-						finallist.append(l-r)
-					if expression[i]=="*":
-						finallist.append(l*r)
+		for i in range(1,len(expression),+1):
+			if expression[i] in "-*+":
+				s1=expression[0:i]
+				s2=expression[i+1:]
+				left=addparentheses(s1,memo)
+				right=addparentheses(s2,memo)
+				for l in left:
+					for r in right:
+						if expression[i]=='+':
+							finallist.append(l+r)
+						if expression[i]=='-':
+							finallist.append(l-r)
+						if expression[i]=="*":
+							finallist.append(l*r)
 		memo[expression]=finallist
 		return finallist
 

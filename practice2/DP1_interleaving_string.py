cbc5702e13e892fe09f570423e6412fe2333a6fe
Nivi-1412 <jyothik.be18@uceou.edu>
2025-09-14 14:52:45 +0530

    feat:dp

--- practice2/DP1_interleaving_string.py
+++ practice2/DP1_interleaving_string.py
@@ -9,16 +9,14 @@ dp=[[0 for _ in range(len(s2)+1)] for _ in range(len(s1)+1)]
 dp[0][0]=True
 
 for i in range(1,len(s1)+1):
-	dp[i][0]=True if s1[:i+1]==s3[:i+1] else False
+	dp[i][0]=True if s1[i-1]==s3[i-1] else False
 
 for j in range(1,len(s2)+1):
-	dp[0][j]=True if s2[:j+1]==s3[:j+1] else False
+	dp[0][j]=True if s2[j-1]==s3[j-1] else False
 
 for i in range(1,len(s1)+1):
 	for j in range(1,len(s2)+1):
-		print(s1[i-1],s3[i+j-2])
 		a=True if s1[i-1]==s3[i+j-1] and dp[i-1][j] else False
-		print(s2[j-1],s3[i+j-2])
 		b=True if s2[j-1]==s3[i+j-1] and dp[i][j-1] else False
 		dp[i][j]=a or b
 

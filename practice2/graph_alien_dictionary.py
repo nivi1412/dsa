ee66c87cca3eb9ebfcb05203cd2ef7e16e83a161
Nivi-1412 <jyothik.be18@uceou.edu>
2025-10-04 21:19:46 +0530

    feat:graph

--- practice2/graph_alien_dictionary.py
+++ practice2/graph_alien_dictionary.py
@@ -7,3 +7,68 @@
 
 # Input: words = ["wrt","wrf","er","ett","rftt"]
 # Output: "wertf"
+
+import ast
+
+def DFS(start_node,graph,result):
+	print(result)
+	if not graph[start_node]:
+		return start_node
+	else:
+		for next_node in graph[start_node]:
+			result=result+DFS(next_node,graph,next_node)
+		return result
+
+
+words=input("enter the words list: ")
+words=ast.literal_eval(words)
+my_list=[]
+
+for i in range(len(words)-1):
+	len1=len(words[i])
+	len2=len(words[i+1])
+	length=min(len1,len2)
+	j=0
+	print(words[i],words[i+1])
+	while(j<length):
+		if words[i][j]!=words[i+1][j]:
+			my_list.append([words[i][j],words[i+1][j]])
+			break
+		j+=1
+print(my_list)
+graph={}
+indegree={}
+is_cycle=False
+
+if my_list:
+	for l in my_list:
+		if l[0] not in indegree:
+			graph[l[0]]=[]
+			indegree[l[0]]=0
+		if l[1] not in indegree:
+			graph[l[1]]=[]
+			indegree[l[1]]=0
+		graph[l[0]].append(l[1])
+		indegree[l[1]]+=1
+	print(graph)
+	print(indegree)
+
+	for letter in indegree:
+		if indegree[letter]==0:
+			start_node=letter
+		else:
+			is_cycle=True
+
+	# result=start_node
+	if not is_cycle:
+		result=DFS(start_node,graph,start_node)
+	else:
+		result=""
+
+	print(result)
+else:
+	print("")
+
+
+
+
--- practice2/graph_min_cost.py
+++ practice2/graph_min_cost.py
@@ -1,2 +1,61 @@
 # graph_min_cost.py
+import ast
+
+ #return min fee
+def min_cost(node,graph,time,fee,visited)->int:
+	# print("time:",time)
+	if graph[node][0][0]==None:
+		if time>=0:
+			return fee
+		else:
+			return -1
+	else:
+		for next_node in graph[node]:
+			# print("node:",next_node[0],next_node[1],next_node[2])
+			# print("fee till now:",fee)
+			if visited[next_node[0]]==False:
+				visited[next_node[0]]=True
+				result=min_cost(next_node[0],graph,time-next_node[1],next_node[2]+fee,visited)
+				# print("result:",result)
+				fee=max(fee,result) if result!=-1 else -1
+		return fee
+
+max_time=int(input("enter the maximum time: "))
+edges=input("enter start,endnodes with the distance: ")
+edges=ast.literal_eval(edges)
+passingfee=input("enter passing fee: ")
+passingfee=ast.literal_eval(passingfee)
+
+graph={}
+visited={}
+
+for edge,fee in zip(edges,passingfee):
+	# print("edge,fee:",edge,fee)
+	if edge[0] not in graph:
+		graph[edge[0]]=[]
+		visited[edge[0]]=False
+	if edge[1] not in graph:
+		graph[edge[1]]=[]
+		visited[edge[1]]=False
+
+	graph[edge[0]].append([edge[1],edge[2],passingfee[edge[1]]])	
+
+for key,values in graph.items():
+	if not values:
+		graph[key].append([None,None,passingfee[key]])
+
+# print(graph)
+node=next(iter(graph))
+visited[node]=True
+visited_copy=visited.copy()
+min_fee=1001
+fee=passingfee[node]
+for next_node in graph[node]:
+	# print("==============")
+	# print(next_node)
+	visited=visited_copy.copy()
+	visited[next_node[0]]=True
+	val=min_cost(next_node[0],graph,max_time-next_node[1],fee+next_node[2],visited)
+	min_fee=min(min_fee,val) if val!=-1 else -1
+print(min_fee)
 
